a07a393784cd91fc1a5c48be0ddaf967061b2c03
IreneRando <irenerandomateo@gmail.com>
2025-06-09 12:20:48 +0200

    Create Users

---
+++ src/Users.jsx
@@ -0,0 +1,5 @@
+export const users = [
+{ id: 1, name: "Juan Pérez", password: "passwordU1", email:"jperez@gmail.com", rol: "cuidador" },
+{ id: 2, name: "Marco Saez", password: "passwordU2", email:"msaez@gmail.com", rol: "familiar" },
+{ id: 3, name: "Elena Saez", password: "passwordU3", email:"esaez@gmail.com", rol: "familiar" },
+];
